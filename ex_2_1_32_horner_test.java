892f46ed65672a7ea70704627e022114493bb463
nekanek <inina.marina@gmail.com>
2013-10-24 09:19:51 -0700

    Update ex_2_1_32_horner_test.java

--- ex_2_1_32_horner_test.java
+++ ex_2_1_32_horner_test.java
@@ -6,7 +6,7 @@ p0 + x (p1 + x( p2 + ... + x(pN-2 + xpN-1))...)
 Write a test client with a static method exp() that uses Horner.eval() to compute an approximation to e^x, using the first N terms of the Taylor series expansion e^x = 1 + x + x ^2/2! + x ^3/3! + .... Include code to check your answer against that computed by Math.exp(). 
 */
 
-public class ex_2_1_32_horner_test
+public class ex_2_1_32_horner_ExpTest
 
 {
         public static void main(String[]args)        
@@ -14,44 +14,27 @@ public class ex_2_1_32_horner_test
 				//test client
                 int x = StdIn.readInt();
 				int N = StdIn.readInt();
-				Double HornerExp = Exp(x, N);
+				
+				Double HornerExp = exp(x, N);
 				Double MathExp = Math.exp(x);
 				
                 System.out.println("e^"+x+"According to Math library equals "+ MathExp+", accroding to Horner's method it equals "+HornerExp+".");
-
-
         }
-        
-        //read matrix
+
         public static double exp(int x, int N)
         {
-                
-                HornerExp = Horner.eval(x, p[])
+                double [] p = CoeffExp(x, N);
+				double HornerExp = ex_2_1_32_horner.eval(x, p);
                 return HornerExp;
         }
         
-        //compute dot product
-        public static double[][] ComputeDotProduct(int N, int M, int K, double[][] A, double[][] B)
+        //compute coefficients of polynomial 4 exponent
+        public static double[] CoeffExp(int x, int N)
         {
-                double[][] Product = new double[N][K];
-                for (int i=0; i<N; i++) {
-                for (int j=0; j<K; j++)        {
-                for (int p=0; p<M; p++)        {
-                        Product[i][j] += (A[i][p] * B[p][j]); }}}
-                return Product;
-        }        
-                        
-                        
-        //print array
-        public static void PrintArray(String ArrayName, int V, int W, double[][] A)
-        {
-                System.out.println(ArrayName);
-                for (int i=0; i<V; i++) {
-                        for (int j=0; j<W; j++)        {
-                                System.out.print(A[i][j] + " ");
-                        }
-                        System.out.println();
-                }
-                System.out.println();
-        }
+			double[] p = new double[N];
+			p[0]=1;
+			for (int i = 1; i<N; i++) {
+				p[i]=p[i-1]/i;}
+            return p;
+        }             
 }
