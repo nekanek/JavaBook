5db69ac9409128bf83019f42a4d176e6edf16701
nekanek <inina.marina@gmail.com>
2013-10-31 12:08:36 -0700

    Update ex_2_2_15_Sicherman.java

--- ex_2_2_15_Sicherman.java
+++ ex_2_2_15_Sicherman.java
@@ -10,39 +10,34 @@ public class ex_2_2_15_Sicherman
    public static void main(String[]args)        
      {
 		//test client
-		double ymin = StdIn.readDouble();
-		double ymax = StdIn.readDouble();
-		int N = StdIn.readInt();
-		double [] a = new double[N];
+		int N = 1000;
+		int SumSicher;
+		int SumStandart;
+		double [] SicherDice1 = {1,3,4,5,6,8};
+		double [] SicherDice2 = {1,2,2,3,3,4};
+		double [] ProbSicher = new double[13];
+		double [] ProbStandart = new double[13];
     
-		for (int i=0; i<N; i++) a[i]= StdIn.readDouble();
-	 
-		double[] ScaledArray = Scale(ymin, ymax, a);
+		for (int i=0; i<N; i++) {
+			SumSicher = SicherDice1[StdRandom.uniform(6)]+SicherDice2[StdRandom.uniform(6)];
+			SumStandart = StdRandom.uniform(1,7)+StdRandom.uniform(1,7);
+			
+			ProbSicher[SumSicher] += 1/n;
+			ProbStandart [SumStandart] += 1/n;
+			
+		}
+	 	Min max mean Prob
+		
 		
 		PrintArray("Given Array: ", a);
 		PrintArray("Scaled Array: ", ScaledArray); 
         }
         
-
- public static double[] Scale(double ymin, double ymax, double[] a)
-  {
-	if (ymin < ymax) {
-		double aMax = StdStats.max(a);
-		double aMin = StdStats.min(a);
-		double [] aScaled = new double[a.length];
-		for (int i = 0; i<a.length; i++) aScaled[i] = ymin + (a[i] -  aMin)*(ymax - ymin)/(aMax- aMin);
-		return aScaled;
-    }
-	else {
-		System.out.println("Min value is bigger or equals Max value.");
-		return a;
-	}
-}
   
  public static void PrintArray(String ArrayName, double[] Array)
   {
 	   System.out.println(ArrayName);
-	   for (int i=0; i<Array.length; i++) 
+	   for (int i=2; i<Array.length; i++) 
 		System.out.print(Array[i] + " "); 
 	   System.out.println();
   }  
