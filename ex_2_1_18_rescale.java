a28cdc5b816bf4e1c87dec095a9d6c233b04b279
Alien <alien47.for.internets@gmail.com>
2013-11-06 21:40:50 +0400

    Add comments about the 2nd task

--- ex_2_1_18_rescale.java
+++ ex_2_1_18_rescale.java
@@ -31,6 +31,14 @@ public class ex_2_1_18_rescale
 			StdOut.print("\n");				
 		}
 		
+	// Actually, min and max are quite popular functions and they both have well-defined semantics.
+	// These functions should get some container as input - the array in our case and return a single number.
+	// It's like math - most math functions do not modify their arguments, instead they compute some new value and
+	// return it. You should do the same. Also it eliminates the need for 2 additional places in the array.
+
+	// There is a certain rule of thumb - Single Responsibility Rule. Each and every thing should do one thing and 
+	// do it good. The input array of N numbers - is a single role. But an array of N numbers and 2 additional numbers
+	// for min and max are 3 roles already. You better use 3 variables instead.
 	public static double[] Minimum(double[] InputArray) 
 		{
 			double Min = InputArray[0];
@@ -54,6 +62,8 @@ public class ex_2_1_18_rescale
 	public static double[] Rescale(double[] InputArray) 
 		{
 			for (int i=0; i<(InputArray.length - 2); i++) {
+				// Here we get division by zero or by extremely small value if (max - min) == epsilon
+				// Try to input the array like {1, 1, 1, 1}
 				InputArray[i]=((InputArray[i]-InputArray[InputArray.length - 2])/(InputArray[InputArray.length - 1]-InputArray[InputArray.length - 2]));	
 			}
 			return InputArray;
