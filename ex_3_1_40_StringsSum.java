2e3d9ccba23b6d4a77d21907fd4fa8d6b45ea219
Neka <inina.marina@gmail.com>
2013-12-04 01:36:51 +0400

    rdy 4 review

--- ex_3_1_40_StringsSum.java
+++ ex_3_1_40_StringsSum.java
@@ -5,11 +5,15 @@
 public class ex_3_1_40_StringsSum {
 
     public static void main(String[] args) {
-    String delimiter = ".";    
+    String delimiter = "\\.";    
     String inputA = args[0]; // 1st string
     String inputB = args[1]; // 2nd string
+
+
     String[] partsOfA = inputA.split(delimiter); // partsOfA[0] - integer part, partsOfA[1] - fraction of inputA
     String[] partsOfB = inputB.split(delimiter);
+    
+
 
 	// append 0 to the shortest integer part
 	String[] zeroedIntegers = AppendZeros (partsOfA[0], partsOfB[0], true);
@@ -28,51 +32,64 @@ public class ex_3_1_40_StringsSum {
 	String SumFractions = SumStrings (partsOfA[1], partsOfB[1]);
 	
 	// adding 1 from fraction to decimal if necessary (if the number of symbols in sum exceeds max number of symbols in fraction part of A or B
-	if ( SumFractions.length() > Math.Max(partsOfA[1].length(),partsOfB[1].length())) { // || ((SumFractions.length() > partsOfB[1].length())) ) {
+	if ( SumFractions.length() > Math.max(partsOfA[1].length(),partsOfB[1].length())) { // || ((SumFractions.length() > partsOfB[1].length())) ) {
 		// generate string of 0s (plus additional symbol in case sum exceeds longest component's length)
 		String fractionToDecimal = "1";
 		
 		String[] zeroedFractionToDecimal = AppendZeros (fractionToDecimal, SumIntegers, true);
+		//StdOut.print(zeroedFractionToDecimal[0]+"   "+zeroedFractionToDecimal[1]+"    ");
+		fractionToDecimal = zeroedFractionToDecimal[0];
+		
 /* 		for (0) {
 			fractionToDecimal = Integer.toString(0)+fractionToDecimal;
 		} */
+		SumIntegers = SumStrings (SumIntegers, fractionToDecimal);
 		
-		SumStrings (IntegerPart, fractionToDecimal)
 		// delete first overflow character from fraction part
 		SumFractions = SumFractions.substring(1,SumFractions.length());
 	}
 	
-	Print
+	StdOut.print(inputA+" + "+ inputB + " = "+ SumIntegers + "."+SumFractions+".\n");
 	}
 
 	public static String SumStrings (String inA, String inB) {
 		
 	    //initialize sumString
 	    String sumString = "";
-	    int[] sumInteger = new int[inA.length()+1]; // plus additional symbol if sum exceeds longest component's length
+	    int[] sumInteger = new int[inA.length()]; 
+	    int firstDigit = 0; // for the case if sum exceeds longest component's length
 	    int Sum;
 //		for (int i = 0; i < inA.length()+1;i++) {
 //			//sumString+= "0";
 //			sumInteger[i]=0;
 //		}
 		
-	    for (int i=inA.length(); i>0; i--) { // don't do it for the most significant 
+	    for (int i=inA.length()-1; i>=0; i--) { // don't do it for the most significant 
 		Sum = Integer.parseInt(String.valueOf(inA.charAt(i)))+Integer.parseInt(String.valueOf(inB.charAt(i)));
 		if (Sum>9) {
 			sumInteger[i]+=(Sum-10);
-			sumInteger[i-1]+=1;//(int)(Sum/10); // there was some reason why i haven't simply added 1 here =/ no idea which it was though v_v
+			if (i!=0) { // exceptional case if sum exceeds longest component's length
+			    sumInteger[i-1]+=1;
+			}
+			else firstDigit += 1;
 		}
 		else if (Sum==9 && sumInteger[i]==1) {
 			sumInteger[i]=0;
-			sumInteger[i-1]+=1;			
+			if (i!=0) {
+			    sumInteger[i-1]+=1;
+			}
+			else firstDigit += 1;			
 		}
 		else {
-			sumInteger[i]+=Sum;	// btw, i can't see how i can move this case up after first "if" without additional condition (for the case Sum==9&&SumInteger!=1) so i decided this way will b better even though the most common case is checked last
+			sumInteger[i]+=Sum;	
 		}
 	    }
 	    // convert array to string
 	    for (int i = 0; i < sumInteger.length;i++) {
 		    sumString+= sumInteger[i];
+	    }
+	    if (firstDigit!=0) {
+		sumString = firstDigit+sumString;
 	    }
 	    return sumString;
 	}
@@ -82,13 +99,15 @@ public class ex_3_1_40_StringsSum {
     public static String[] AppendZeros (String A, String B, boolean inBeginning) {	
 	    
 	    if (A.length()<B.length()) {
-		    for (int i = 0; i<B.length()-A.length(); i++) {
+		    int n = B.length()-A.length();
+		    for (int i = 0; i<n; i++) {
 			if (inBeginning==true) A="0"+A; 
 			else A=A+"0";
 		    }
 	    }	
 	    else if (A.length()>B.length()) {
-		    for (int i = 0; i<A.length()-B.length(); i++) {
+		    int n = A.length()-B.length();
+		    for (int i = 0; i<n; i++) {
 			if (inBeginning==true) B="0"+B; 
 			else B=B+"0";
 		    }
