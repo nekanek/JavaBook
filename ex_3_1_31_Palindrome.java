db27b782abfbce5b99b5f7bbb3d5ac82f2b39c36
nekanek <inina.marina@gmail.com>
2013-12-05 15:23:55 +0300

    rdy 4 review

--- ex_3_1_31_Palindrome.java
+++ ex_3_1_31_Palindrome.java
@@ -1,75 +1,81 @@
 /*
 3.1.31. Complemented palindrome detector. In DNA sequence analysis, a complemented palindrome is a string equal to its reverse complement. Adenine (A) and Thymine (T) are complements, as are Cytosine (C) and Guanine (G). For example, ACGGT is a complement palindrome. Such sequences act as transcription-binding sites and are associated with gene amplification and genetic instability. Given a text input of N characters, find the longest complemented palindrome that is a substring of the text. For example, if the text is GACACGGTTTTA then the longest complemented palindrome is ACGGT. Hint: consider each letter as the center of a possible palindrome of odd length, then consider each pair of letters as the center of a possible palindrome of even length.
+
+N: another sequences to check: GACACGGTTTT(AGACACGGTGTCT)TA
+GACA(AGACACGTGTCT)CGGTTTT(AGACACGTGTCTAGACACGTGTCT)TATTTTT(AGACACGTGTCTGAGACACGTGTCT)
+GACAAGACACGTGTCTCGGTTTT(AGACACGTGTCTAGACACGTGTCT)TATTTTT(AGACACGTGTCTGAGACACGTGTCT)
+GACAAGACACGTGTCTCGGTTTTAGACACGTGTCTAGGTGTCTTATTTTT(AGACACGTGTCTAGACACGTGTCT)
+GACAAGACACGTGTCTCGGTTTTTTTAGACACGTGTCTAGACACGTGTCTTTTTATTTTTAGACACGTGTCTGAGACACGTGTCT
+GACAAGACACGTGTCTCGGTTTTAGACACGTGTCTAGACACGTGTCTTATTTTTAGACACGTGTCTGAGACACGTGTCT
 */
 
 public class ex_3_1_31_Palindrome {
 
     public static void main(String[] args) {
-    String dilimiter = ".";    
-    String inputA = args[0]; // 1st string
-	String inputB = (args[1]); // 2nd string
-	String[] partsOfA = inputA.split(delimiter); // partsOfA[0] - integer part, partsOfA[1] - fraction of inputA
-	String[] partsOfB = inputB.split(delimiter);
-	
-	// append 0 to the shortest integer part
-	if (partsOfA[0].length()<partsOfB[0].length()) {
-		for (int i = 0; i<partsOfB[0].length()-partsOfA[0].length(); i++) {
-			partsOfA[0]+="0"; 
-		}
-	}	
-	else if (partsOfA[0].length()>partsOfB[0].length()) {
-		for (int i = 0; i<partsOfA[0].length()-partsOfB[0].length(); i++) {
-			partsOfB[0]+="0"; 
-		}
-	}	
-	
-	// calculate  sum of integer parts
-	SumStrings (IntegerPartA, IntegerPartB)
-	
-	// calculate sum of fractions
-	SumStrings (fractionPartA, fractionPartB)
-	
-	// adding 1 from fraction to decimal if necessary
-	if (fraction[0]==1) {
-		// generate string of 1 plus 1 symbol if sum exceeds longest component's length
-		String fractionToDecimal = "1";
-		for 	{
-			fractionToDecimal = Integer.toString(0)+fractionToDecimal;
-		}
-		SumStrings (IntegerPart, fractionToDecimal)
-	}
-	
-	Print
-	}
 
-	public static String SumStrings (String inA, String inB) {
+	// read dna sequence
+	String dna = args[0];
+	
+	int MaxLength = 0;
+	int MaxLengthBuf = 0;
+	int MaxIndex = 0;
+	
+	// palindrome of odd length
+    // check from each symbol i to the left and right	
+	for (int i=1; i<dna.length()-1; i++) {
 		
-		//initialize sumString
-		String sumString = "";
-		int[] sumInteger = new int[inA.length()+1] // plus additional symbol if sum exceeds longest component's length
-		for (int i = 0; i < inA.length()+1;i++) {
-			sumString+= "0";
-			sumInteger[i]=0;
+		MaxLengthBuf=1;
+		// j - offset to the left or right
+		outerloop:
+		for (int j=1; j<Math.min(i,dna.length()-i); j++) {
+			switch (dna.charAt(i-j)) {
+				case 'A': if (dna.charAt(i+j)=='T') {MaxLengthBuf+=2;break;} 
+							else {break outerloop;}
+				case 'T': if (dna.charAt(i+j)=='A') {MaxLengthBuf+=2;break;} 
+							else {break outerloop;}
+				case 'C': if (dna.charAt(i+j)=='G') {MaxLengthBuf+=2;break;} 
+							else {break outerloop;}
+				case 'G': if (dna.charAt(i+j)=='C') {MaxLengthBuf+=2;break;} 
+							else {break outerloop;}
+				default: {StdOut.print("Symbol "+ dna.charAt(i-j) + " can't be in DNA sequence.\n"); System.exit(0); } 
+			}
 		}
 		
-	for (i=n; i>=0; i--	) {
-		int Sum= Integer.parseInt(inA.charAt(i))+Integer.parseInt(inB.charAt(i));
-		if (Sum>9) {
-			SumInteger[i]+=(Sum-10);
-			SumInteger[i-1]+=(int)(Sum/10);
-		}
-		else if (Sum==9&&SumInteger[i]==1) {
-			SumInteger[i]=0;
-			SumInteger[i-1]+=1;			
-		}
-		else {
-			SumInteger[i]+=Sum;	// btw, i can't see how i can move this case up after first "if" without additional condition (for the case Sum==9&&SumInteger!=1) so i decided this way will b better
+		if (MaxLength<MaxLengthBuf) {
+			MaxLength = MaxLengthBuf;
+			MaxIndex = i;
 		}
 	}
-
-	// convert array to string
 	
-	return SumString;
+	// palindrome of even length
+	for (int i=0; i<dna.length()-1; i++) {
+		MaxLengthBuf=0;
+		
+		// j - offset to the left or right
+		outerloop:
+		for (int j=1; j<Math.min(i+2,dna.length()-i); j++) {
+			switch (dna.charAt(i+1-j)) {
+				case 'A': if (dna.charAt(i+j)=='T') {MaxLengthBuf+=2;break;} 
+							else {break outerloop;}
+				case 'T': if (dna.charAt(i+j)=='A') {MaxLengthBuf+=2;break;} 
+							else {break outerloop;}
+				case 'C': if (dna.charAt(i+j)=='G') {MaxLengthBuf+=2;break;} 
+							else {break outerloop;}
+				case 'G': if (dna.charAt(i+j)=='C') {MaxLengthBuf+=2;break;} 
+							else {break outerloop;}
+				default: {StdOut.print("Symbol "+ dna.charAt(i+1-j) + " can't be in DNA sequence.\n"); System.exit(0); } 
+			}
+		}
+		if (MaxLength<MaxLengthBuf) {
+			MaxLength = MaxLengthBuf;
+			MaxIndex = i+1;
+		}		
+	}	
+	
+	// print result
+	StdOut.print("Maximum palindrome of size "+ MaxLength + " starts at "+ (MaxIndex-(int)MaxLength/2+1) + "' symbol. Palindrome:\n");
+	StdOut.println(dna.substring(MaxIndex-(int)MaxLength/2,MaxIndex-(int)MaxLength/2+MaxLength));
+	
     }   
 
 }
