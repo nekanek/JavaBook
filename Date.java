d8b544578a1cb278b99972c9e30ed0658d924bfb
nekanek <inina.marina@gmail.com>
2014-01-08 21:20:08 +0300

    Update Date.java

--- Date.java
+++ Date.java
@@ -3,157 +3,249 @@ Dates. Create a data type Date that represents a date. You should be able to cre
 */
 
 public class Date {
-    private final String mm;  
-    private final String dd;   
-	  private final String year;
+    
+    private final int dd;
+    private final int mm;
+    private final int year;
 	
-    public Date(String mm, String dd, String year)
+    public Date(int da, int mo, int yr)
 	{
 		// check validity of entered date
-		if ((mm<1)||(mm>12)||(dd<1)||(dd>31)||(((mm=="02")||(mm=="2"))&&(((dd>29)&&(year%4==0))||((dd>28)&&(year%4!=0))))||(year<0)) {
+		boolean lpYear = (((yr%4 == 0) && (yr%100 != 0)) || (yr%400 == 0));
+		if ((mo<1)||(mo>12)||(da<1)||(da>31)||(yr<0)||((mo==2)&&(da>29)&& lpYear)||((mo==2)&&(da>28)&&(!lpYear))) {
 			throw new RuntimeException("Illegal date. Please, check.");
 		}
-		else if (divisor<0) {
-			p = -dividend; // works both for -p/-q and p/-q
-			q = -divisor;
-		}
+
 		else {
-			p = dividend; 
-			q = divisor;	
+			mm = mo; 
+			dd = da;
+			year = yr;
 		}
 	}
 
-    // return a string representation of the invoking Rational object
-    public String toString() {
-		/*         if (q == 0) { 
-			double inf = Double.POSITIVE_INFINITY; 
-			return Double.toString(inf);
-		} */
-		if (p == 0) return "0";
-		else if (q==1) return ""+p;
-        else return p + "/" + q;
+
+    public String toString() {	
+        return dd + "/"+mm + "/"+year;
     }
-
-     // return a + b   
-    public Rational plus(Rational b) {
-        long dividend = p*b.q + q*b.p;
-        long divisor = q*b.q;
-		Rational c = new Rational(dividend, divisor);
-		return c.reduce();
+    
+    public String printDDMMYYYY() {	
+        if (dd < 10) {
+	    if (mm < 10) {
+		return "0" + dd + "/" + "0" + mm + "/" + year;
+	    }
+	    else return "0" + dd + "/" + mm + "/" + year;
+	}
+	else if (mm < 10) {
+	    return dd + "/" + "0" + mm + "/" + year;
+	} 
+	else return dd + "/" + mm + "/" + year;
     }
-
-    // return a - b
-    public Rational minus(Rational b) {
-        long dividend = p*b.q - q*b.p;
-        long divisor = q*b.q;
-		Rational c = new Rational(dividend, divisor);
-		return c.reduce();
+    
+    public boolean leapYear() {
+	if ( ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0) ) return true;
+	else return false;
+	    
     }
-
-    // return a * b
-    public Rational times(Rational b) {
-        long dividend = p*b.p;
-        long divisor = q*b.q;
-		Rational c = new Rational(dividend, divisor);
-		return c.reduce();
+    
+    public int daysInMonth() {
+	int days = 0;
+	switch(mm) {
+	    case 1:
+		days = 31; break;
+	    case 2:
+		if (leapYear()) days = 29; 
+		else days = 28;
+		break;
+	    case 3:
+		days = 31; break;
+	    case 4:
+		days = 30; break;
+	    case 5:
+		days = 31; break;
+	    case 6:
+		days = 30; break;
+	    case 7:
+		days = 31; break;
+	    case 8:
+		days = 31; break;
+	    case 9:
+		days = 30; break;
+	    case 10:
+		days = 31; break;
+	    case 11:
+		days = 30; break;
+	    case 12:
+		days = 31; break;
+	}
+	return days;
     }
+    
 
-    // return a / b
-    public Rational divide(Rational b) {
-        long dividend = p*b.q;
-        long divisor = q*b.p;
-		Rational c = new Rational(dividend, divisor);
-		return c.reduce();
-    }
+    public int daysPassed(Date b) {
+//	dd mm year;
+//	b.dd b.mm b.year;
+	Date minDate = null;
+	Date maxDate = null;
+	int days = 0; // counting days
 
-    // return the dividend or divisor
-    public double p() { return p; }
-    public double q() { return q; }
+	if (equals(b)) return days;
+	else if (older(b)) {
+	    minDate = new Date(b.dd, b.mm, b.year);
+	    maxDate = new Date(dd, mm, year);
+	}
+	else {
+	    minDate = new Date(dd, mm, year);
+	    maxDate = new Date(b.dd, b.mm, b.year);   	
+	}
 
-	
-
-    // raising to a power
-    public Rational power(int x) {
-    	Rational c = new Rational((long)Math.pow(p,x), (long)Math.pow(q,x));
-		return c.reduce();
+	// add days of year if there is at least 1 whole year between dates
+	if ((maxDate.year-minDate.year)>1) {
+	    for (int y = maxDate.year; y > minDate.year; y--) {
+		Date tempDate = new Date(1,1,y);
+		if (tempDate.leapYear()) days += 356;
+		else days += 355;
+	    }	    
 	}
 	
+	// add days of month from min date till the end of the year or till max date
+	if (maxDate.older(minDate.endOfYear())) {
+	    // maxDate is in another year, counting till the end of min year
+	    // counting days in full months
+	    if ((12 - minDate.mm) > 0) {
+		for (int m = 12; m > minDate.mm; m--) {
+		    Date tempDate = new Date(1,m,minDate.year);
+		    days += tempDate.daysInMonth();
+		}
+	    }
+	    // counting days of minDate month
+	    days += (minDate.daysInMonth()-minDate.dd);
+	    
+	    // counting days in maxDate year
+	    // counting days of maxDate month
+	    days += (maxDate.dd);	    
+	    if (maxDate.mm > 1) {
+		    for (int m = 1; m < maxDate.mm; m++) {
+			Date tempDate = new Date(1,m,maxDate.year);
+			days += tempDate.daysInMonth();
+		    }
+	    }
 
-/*     public Rational root(int x) {
-        return new Complex(Math.power(p,1/x), Math.power(q,1/x));
-    } */
-	
-	
-    // return value of e^Rational
-    public Double exp() {
-		//Double e = Math.pow(Math.E,p/q);
-        return Math.pow(Math.E,(p/q));
+	}    
+	else {
+	    // max date is in the same year as minDate, counting days till max date
+	    if (minDate.mm == maxDate.mm) {
+		days += maxDate.dd-minDate.dd;
+	    }
+	    else {
+	    // in different monthes (but same year)
+		// counting days in full months
+		if ((maxDate.mm - minDate.mm) > 1) {
+		    for (int m = (maxDate.mm - 1); m > minDate.mm; m--) {
+			Date tempDate = new Date(1,m,minDate.year);
+			days += tempDate.daysInMonth();
+		    }
+		}
+		// counting days of minDate month
+		days += (minDate.daysInMonth()-minDate.dd);	
+
+		// counting days of maxDate month
+		days += (maxDate.dd);
+	    }
+	}
+	return days;
+    }
+    
+    public boolean older(Date b) {
+	if (year > b.year || ((year == b.year) && ((mm > b.mm) || ((mm == b.mm) && (dd >= b.dd))))) return true;
+	else return false;
+    }
+   
+    public boolean equals(Date b) {
+	if (dd == b.dd && mm == b.mm && year == b.year) return true;
+	else return false;
+    }
+    
+    public Date endOfYear() {
+	return new Date(31,12,year);
+    }  
+    
+    public String dayOfWeek() {
+	String weekDay = "";
+	int remainder = 0;
+	if (year >= 1990) {
+	    Date tempDate = new Date (1,1,1990);
+	    remainder = daysPassed(tempDate)%7;
+	}
+	else if (year > 1900) {
+	    
+	}
+	else if (year > 0) {
+	    
+	}
+	switch(remainder) {
+	    case 0:
+		weekDay = "Monday"; break; 	    
+	    case 1:
+		weekDay = "Monday"; break; 
+	    case 2:
+		weekDay = "Monday"; break; 
+	    case 3:
+		weekDay = "Monday"; break; 
+	    case 4:
+		weekDay = "Monday"; break; 
+	    case 5:
+		weekDay = "Monday"; break; 
+	    case 6:
+		weekDay = "Monday"; break; 
+
+	}
+	return weekDay;
     }
 
-	// absolute value of a number
-    public Rational abs() {
-		if (p>=0) return  new Rational(p,q);
-		else return  new Rational(-p,q);
+    public Date addDays(int days) {
+
+	int newDay = dd;
+	int newMonth = mm;
+	int newYear = year;
+	Date tempDate = new Date (newDay, newMonth, newYear);
+	
+	while (days > (tempDate.daysInMonth()-newDay)) {
+	    days -= daysInMonth()-newDay;
+	    if (newMonth == 12) {
+		newMonth = 1;
+		newYear += 1;
+	    }
+	    else newMonth += 1;
+	    
+	    newDay = 1;
+	    tempDate = new Date (newDay, newMonth, newYear);
+	}
+	newDay += days;
+	tempDate = new Date (newDay,newMonth,newYear);
+	return tempDate;
     }	
-	
-    // return a double value sin(Rational)
-    public Double sin() {
-        return Math.sin(p/q);
-    }
-
-    // return a double value sin(Rational)
-    public Double cos() {
-        return Math.cos(p/q);
-    }
-
-    // return a double value sin(Rational)
-    public Double tan() {
-        return sin()/cos();
-    }
-	
-
-	// return a new Rational object whose value is reduced value of the same rational
-    public Rational reduce() {
-		// find the greatest common divisor
-		long Min = Math.min(Math.abs(p), q);
-		for (long i = Min; i>0; i--) {
-			if  (((q%i)==0)&&((p%i)==0)) 
-				return new Rational(p/i, q/i);	
-		}
-		Rational a = new Rational(p, q);
-		return a;
-    }   
-	
-    // return true if both denominator and divisor are equivalent for two objects	
-	public boolean equals(Rational b) {
-        return (p==b.p&&q==b.q);
-    }    
+  
 
     // sample client for testing
     public static void main(String[] args) {
-        Rational a = new Rational(1, 1);
-        Rational b = new Rational(-4, 9);
-
-        System.out.println("a            = " + a);
-        System.out.println("b            = " + b);
- 
-        System.out.println("b + a        = " + b.plus(a));
-		System.out.println("a - b        = " + a.minus(b));
-        System.out.println("a * b         = " + a.times(b));
-        System.out.println("a / b         = " + a.divide(b));
-		
-		System.out.println("a equals b is " + b.equals(a));
-		System.out.println("toString method check: a is "+ a.toString());
-
-        System.out.println("dividend of b = " + b.p());
-        System.out.println("divisor of a  = " + a.q());
-        System.out.println("a ^ 3         = " + a.power(3));		
-        System.out.println("e ^ a         = " + a.exp());	      
-        System.out.println("|a|          = " + a.abs());
-
-        System.out.println("sin(a)       = " + a.sin()); 
-        System.out.println("cos(a)       = " + a.cos()); 
-        System.out.println("tan(a)       = " + a.tan()); 		
+        Date a = new Date(1, 1,2013);
+        Date b = new Date(Integer.parseInt(args[0]), Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+	
+        System.out.println("a            = " + a.printDDMMYYYY());
+        System.out.println("b            = " + b.toString());
+	System.out.println("Days passed: " + a.daysPassed(b));
+	
+	if (b.leapYear()) System.out.println("b is a leap year.");
+	else System.out.println("b isn't a leap year.");
+	
+	if (a.equals(b)) System.out.println("Dates a and b are the same.");
+	else if (a.older(b)) System.out.println("Date a is older."); 
+	else System.out.println("Date b is older."); 
+	
+	System.out.println("60 days after " + b + " it will be " + b.addDays(60) + ".");
+	System.out.println(b + " is " + b.dayOfWeek() + ".");
     }
+    
+
 
 }
