26be04fb9141408a57e8bd7b3f2503fa2567774f
nekanek <inina.marina@gmail.com>
2014-02-13 10:37:56 -0500

    Update SparseVector.java

--- SparseVector.java
+++ SparseVector.java
@@ -8,8 +8,8 @@
 public class SparseVector {
 
     private final int length;         // ? length of the vector
-    private int[] values;       // array of vector's components
-    private int[] indexes;       // array of indexes of those components
+    private int[] values;           // array of vector's components
+    private int[] indexes;       // array of indexes of corresponding components
     
     private static final int DEFAULT_LENGTH = 100;
     private static final int[] DEFAULT_VALUES = {100, 66, 567, 1};
@@ -18,40 +18,52 @@ public class SparseVector {
     // constructors:
     public SparseVector() {
         this(DEFAULT_LENGTH, DEFAULT_VALUES, DEFAULT_INDEXES);
-
     }   
-    0
+    
     public SparseVector(int length, int[] values, int[] indexes) {
         this.length = length;
         this.values = values;
         this.indexes = indexes;
+    
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // create a vector from an array
-   public SparseVector(double[] data) {
+    // create sparse vector from an array
+    public SparseVector(int[] data) {
         N = data.length;
-
         // defensive copy so that client can't alter our copy of data[]
-        this.data = new double[N];
-        for (int i = 0; i < N; i++)
-            this.data[i] = data[i];
+        this.values = new int[N];
+        this.indexes = new int[N];
+        int index = 0;
+        for (int i = 0; i < N; i++) {
+            if (data[i] != 0) {
+                this.values[index] = data[i];
+                this.index[index] = i+1;
+                index++;
+            }
+        }
     }
 
+    
+    // recreate a vector from sparse vector
+    public int[] RecreateVector() {
+        int[] vector = new int[length];
+        int index = 0;
+        
+        for (int i=0; i<length; i++) {
+            if (indexes[index] == i) {
+                vector[i] = values[i];
+                index++;
+            }
+        }
+        return vector;
+    }
+
+    
     public int dotProduct(SparseVector b) {
         
-        // check if (this.length != b.length) throw new exception..
-        
-        int dot = 0;
+        if (this.length != b.length) throw new RuntimeException("Dimensions don't agree");
+
+        int dotSum = 0;
         int indexA = 0;
         int indexB = 0;
         for (int i=0; i<this.length; i++) {
@@ -59,36 +71,73 @@ public class SparseVector {
                 indexA++;
                 if (b.indexes[indexB] == i) {
                     indexB++;
-                    dot += this.values[indexA-1]*b.values[indexB-1];
+                    dot +=  this.values[indexA-1]*b.values[indexB-1];
                 }
             }
         }
     }
-   
+
+    public int Plus(SparseVector b) {
+        int N = this.length;
+        if (N != b.length) throw new RuntimeException("Dimensions don't agree");
+        
+        int BufferLength = this.values.length+b.values.length;
+        int[] SumValues = new int[BufferLength];
+        int[] SumIndexes = new int[BufferLength];
+        SumIndexes[indexSum] =  i;
+
+        
+        int indexA = 0;
+        int indexB = 0;
+        int indexSum = 0;
+        boolean newSumValue = false;
+        
+        
+        for (int i = 0; i < N; i++) {
+            if (this.indexes[indexA] == i) {
+                SumValues[indexSum] +=  this.values[indexA];
+                SumIndexes[indexSum] =  i;
+                indexA++;
+                newSumValue = true;
+            }
+            if (b.indexes[indexB] == i) {
+                SumValues[indexSum] +=  b.values[indexB];
+                SumIndexes[indexSum] =  i;
+                indexB++;
+                newSumValue = true;
+            }
+            if (newSumValue) {
+                indexSum++;
+                newSumValue = false;
+            }
+        }
+        
+        for (j=BufferLength-1; j>-1; j--) {
+            if (SumValues[j] != 0) {
+                int SumSparseVectorLength = j+1;
+            }
+        }
+        
+        
+        SparseVector Sum = Sparsevector(SumSparseVectorLength, new int[N], new int[N]);
+        return Sum;
+    }
+    
     // + check whether right angle? (if dotP=0)
    
     // return the length of the vector
     public int length() {
-        return N;
+        return length;
     }
 
-    // return the inner product of this Vector a and b
-    public double dot(Vector that) {
-        if (this.N != that.N) throw new RuntimeException("Dimensions don't agree");
-        double sum = 0.0;
-        for (int i = 0; i < N; i++)
-            sum = sum + (this.data[i] * that.data[i]);
-        return sum;
-    }
+
    
    
     // test client
     public static void main(String[] args) {
-        double[] xdata = { 1.0, 2.0, 3.0, 4.0 };
-        double[] ydata = { 5.0, 2.0, 4.0, 1.0 };
-
-        Vector x = new Vector(xdata);
-        Vector y = new Vector(ydata);
+        
+        SparseVector x = new SparseVector();
+        SparseVector y = new SparseVector(0,5,0,0,0,6,7,8,0,0,0,0,0,0,6);
 
         System.out.println("x        =  " + x);
         System.out.println("y        =  " + y);
