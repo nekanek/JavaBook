e36195fa35262958939b14acb13c5fd76385ce5c
nekanek <inina.marina@gmail.com>
2014-02-14 09:21:14 -0500

    Update Genome.java

--- Genome.java
+++ Genome.java
@@ -7,11 +7,63 @@ GACAAGACACGTGTCTCGGTTTT(AGACACGTGTCTAGACACGTGTCT)TATTTTT(AGACACGTGTCTGAGACACGTGT
 GACAAGACACGTGTCTCGGTTTTAGACACGTGTCTAGGTGTCTTATTTTT(AGACACGTGTCTAGACACGTGTCT)
 GACAAGACACGTGTCTCGGTTTTTTTAGACACGTGTCTAGACACGTGTCTTTTTATTTTTAGACACGTGTCTGAGACACGTGTCT
 GACAAGACACGTGTCTCGGTTTTAGACACGTGTCTAGACACGTGTCTTATTTTTAGACACGTGTCTGAGACACGTGTCT
+
+Genome. Implement a data type to store the genome of an organism. Biologists often abstract away the genome to a sequence of nucleotides (A, C, G, or T). The data type should support the 
+method addNucleotide, nucleotideAt(int i), and addCodon. 
+check that only legal nucleotides are added, can change to more time or memory efficient implementation without affecting client.
+
+    StringGenome.java has one instance variable of type String. It implements addNucleotide with string concatenation. Each method call takes time proportional to the size of the current genome. Not practical spacewise either for large genomes since nucleotide is stored as a 16-bit char.
+    
+Genome.java implements a genome as an array of characters. The size of the array is doubled when the array fills up. The method addNucleotide is now constant time. Space consumption is still 16 bits per nucleotide.
+    
+    CompactGenome.java implements a genome as boolean array. We need to use two bits per nucleotide since there are 4 different nucleotides. As in the previous implementation, we use a dynamic array with repeated doubling. Now, each nucleotide consumes 2 bits of storage (instead of 16). 
+битовые операции используются для того чтобы хранить 4 двухбитовых значения. для модификации отдельных значений нужно применять битовые операции над типом int. типа прочитать первые два бита, изменить биты 4 и 5..
+    
+    
+Забавно, что в creative 14 вариант решения compact genome - неверный. Точнее он работает, но неверно то, что о нём пишет автор. Там далеко не 2 бита на букву, а те же самые 16. Я не помню, ты ведь проходила битовые операции над целыми числами? А то я бы предлжил правильно перепилить этот пример, чтобы он задействовал лишь честные 2 бита на букву. Могу объяснить подробнее, как это сделать.
+
+все символы, без проверки на кодонность
+addNucl - добавляет одну букву
+addCodon - добавить кодон
 */
+package JavaBook;
+import JavaBook.stanfStd.*;
 
+public class Genome {
 
-public class ex_3_1_31_Palindrome {
-
+    private final char[] Values;
+    private final int Length;
+    
+     private static final char[] DEFAULT_VALUES = {'A', 'T', 'G', 'C'};
+     // private static final int DEFAULT_LENGTH = 10;
+    
+    // constructors:
+    public Genome() {
+        this(DEFAULT_VALUES);
+    }   
+    
+    public Genome(char[] values) {
+        this.length = 2*values.length;
+        this.values = values;
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public static void main(String[] args) {
 
 	// read dna sequence
