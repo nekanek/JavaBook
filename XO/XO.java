b39b53fd575d19a2e206ad4d0f1878fa0b5e4ea6
nekanek <inina.marina@gmail.com>
2014-03-15 08:52:01 +0300

    Update XO.java

--- XO/XO.java
+++ XO/XO.java
@@ -9,8 +9,19 @@ public class XO {
     public static void main(String[] args) {
         sys "Welcome to game of XO. \nPlease, choose game mode:\n1) Human vs Human\n2)...\n "
         read input
-        create players
-        beginGame
+        // create players
+        switch
+        
+        // read field size
+        prompt and read
+        int size = 
+        if size is ok
+            GameOfXO myGame = new GameOfXO(size);
+        else {
+            "Your input was wrong, beginning default game 3x3."
+            GameOfXO myGame = new GameOfXO();
+        }
+        myGame.beginGame();
         sys "tu"
 
     }
