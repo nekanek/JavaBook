6ca75c366c41d4447e410a7cc4cadca4fd4e1db7
nekanek <inina.marina@gmail.com>
2014-03-21 18:38:54 +0300

    Update Human.java

--- XO/Human.java
+++ XO/Human.java
@@ -3,7 +3,11 @@ package JavaBook.XO;
 
 
 public class Human extends Player {
-    public boolean isAI;
+    private final boolean isAI; // = false;
+    
+    public boolean getIsAI() {
+        return isAI;
+    }
     
     public Human() {
         this.isAI = false;
@@ -12,11 +16,12 @@ public class Human extends Player {
     @Override
     public int[] returnCoordinates(char[][] board) {
         System.out.println("Please, make yr move; enter coordinates [x y].");
-        String input1 = "1 1"; // GameOfXO.readInput;
-
         int[] input = new int[2];
-        input[0] = 1;// Integer.parseinteger(inputString.substring(0)); // make it after space to take into account size > 9
-        input[1] = 1;//Integer.parseinteger(inputString.substring(2)); // make it after space to take into account size > 9
+        input = XO.scanIntArray(); // GameOfXO.readInput;
+
+        //int[] input = new int[2];
+        //input[0] = 1;// Integer.parseinteger(inputString.substring(0)); // make it after space to take into account size > 9
+        //input[1] = 1;//Integer.parseinteger(inputString.substring(2)); // make it after space to take into account size > 9
         
         // check for null - make it diff method
         
