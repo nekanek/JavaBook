052e0223b598314d93ba464ac5958892293a30d3
nekanek <inina.marina@gmail.com>
2014-03-28 17:09:03 +0300

    Create Comparable.java

---
+++ Algo_4/Comparable.java
@@ -0,0 +1,25 @@
+
+package JavaBook.Algo_4;
+
+import java.util.Scanner;
+
+
+public class Domain implements Comparable {
+
+    private static final char SEPARATOR = '.';
+    private static final int MAX_NUMBER_OF_DOMAINS = 30;
+    private String[] DomainName;
+    
+    public Domain(String input) {
+    
+        for (int i = 0; i < MAX_NUMBER_OF_DOMAINS; i++) {
+            
+            
+        }
+    }
+    
+
+
+}
+
+
