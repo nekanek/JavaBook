8fa8f2da566f63ae7426ccbffb482e21938b83af
nekanek <inina.marina@gmail.com>
2014-06-26 23:49:48 +0400

    BTree simple version

--- Algo_4/BTrees_4_4/BTree.java
+++ Algo_4/BTrees_4_4/BTree.java
@@ -11,18 +11,17 @@ BT.1.    Implement the following methods for a binary tree that each take as arg
 
 package JavaBook.Algo_4.BTrees_4_4;
 
-public class BTree<K extends Comparable & Iterable<K>, V> {
+public class BTree<V> {
 
-    private final Node root;        
-    //private int size;             
+    private final Node root;                    
 
     private class Node {
-        K key;
+        int key;
         V value;
         Node left;
         Node right;
         
-        private Node(K in_key, V in_value) {
+        private Node(int in_key, V in_value) {
             this.key = in_key;
             this.value = in_value;
         }
@@ -30,30 +29,60 @@ public class BTree<K extends Comparable & Iterable<K>, V> {
     
     public BTree() {
         root = null;
-        //size = 0;
     }
     
-    public BTree(K in_key, V in_value) {
+    public BTree(int in_key, V in_value) {
         root = new Node(in_key, in_value);
-        //size = 0;
     }
     
     public int size() {
+        return size(this.root);
+    }
+    
+    private int size(Node _root) {
         int size = 1;
-        if(this.root.left != null) {
-            
+        if(_root.left != null) {
+            size += size(_root.left);
         }
-        if(this.root.right != null) {
-            
+        if(_root.right != null) {
+            size += size(_root.right);
         }       
         return size;
     }
 
-    public int leaves(BTree root) {
-     
+    public int leaves() {
+        return leaves(this.root);
+    }
+    
+    
+    private int leaves(Node _root) {
+        int leaves = 0;
+        if (_root.left == null && _root.right == null) {
+            leaves++;
+        }
+        else{
+            if(_root.left != null) {
+                leaves += leaves(_root.left);
+            }
+            if(_root.right != null) {
+                leaves += leaves(_root.right);
+            } 
+        }
+        return leaves;
     }
 
-    public int total(BTree root) {
-   
+    public int total() {
+        return total(this.root);
+    }    
+    
+    private int total(Node _root) {
+        int sum = _root.key;
+        if(_root.left != null) {
+            sum += total(_root.left);
+        }
+        if(_root.right != null) {
+            sum += total(_root.right);
+        }       
+        return sum;   
     }   
 }    
