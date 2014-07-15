f3ef8904be576c3176ff45cf0f176d3557a174a9
nekanek <inina.marina@gmail.com>
2014-07-15 23:58:20 +0400

    Ring buffer is rdy 4 review

--- Algo_4/StacksQueues_4_3/RingBuffer.java
+++ Algo_4/StacksQueues_4_3/RingBuffer.java
@@ -2,6 +2,13 @@
 4.3.7. Ring buffer. A ring buffer or circular queue is a FIFO data structure of a fixed size N. It is useful for transferring data between asynchronous processes or storing log files. When the buffer is empty, the consumer waits until data is deposited; when the buffer is full, the producer waits to deposit data. A ring buffer has the following methods: 
 isEmpty(), isFull(), enqueue(), and dequeue(). 
 Write an generic data type RingBuffer using an array (with circular wrap-around for efficiency). 
+
+ RingBuffer может быть как фифо, так и лифо. Тут просят фифо. Для этого надо контролить и точку вставки, и точку чтения. wrap-around означает, что когда ты достигаешь конца массива в добавлении, то надо начинать вставлять сначала. И так до бесконечности. При этом надо помнить позицию самого старого неперезаписанного элемента.
+
+abcdfghjkl
+1234567890
+
+
 */
 
 package JavaBook.Algo_4.StacksQueues_4_3;
@@ -9,30 +16,39 @@ package JavaBook.Algo_4.StacksQueues_4_3;
 public class RingBuffer {
 
     private int[] values;
-    private int last;
+    private int lastAdded;
+    private int firstToRead;
     
     public RingBuffer(int size) {
         this.values = new int[size];
         this.last = -1;
+        this.firstToRead = -1;
     }
-    public RingBuffer(int[] _values) {
-        this(_values, _values.length);
-    }    
+
+    // public RingBuffer(int[] _values) {
+    //     this(_values, _values.length);
+    // }    
     
-    public RingBuffer(int[] _values, int size) {
-        this.values = new int[size];
-        for (int i = 0; i < _values.length; i++) {
-            this.values[i] = _values[i]; 
-        }
-        this.last = _values.length -1;
-    }
+    // public RingBuffer(int[] _values, int size) {
+    //     this.values = new int[size];
+    //     for (int i = 0; i < _values.length; i++) {
+    //         if (_values[i] == 0) {
+    //             this.last = i-1;
+    //             break;
+    //         }
+    //         this.values[i] = _values[i]; 
+    //     }
+
+    //     this.last = _values.length -1;
+    // }
     
     
     public boolean isEmpty() {
-        return (last == -1);
+        return (lastAdded == -1 || firstToRead == -1);
     }
+
     public boolean isFull() {
-        return last == (values.length - 1);
+        return (lastAdded + 1)%values.length == firstToRead;
     }
     
     public void enqueue(int item){
@@ -40,7 +56,13 @@ public class RingBuffer {
             System.out.println("Failed to add item, buffer is full.");
         }
         else {
-            values[++last] = item;
+            if (lastAdded == values.length - 1) {
+                values[0] = item;
+                lastAdded = 0;    
+            }
+            else {
+                values[++lastAdded] = item;
+            }
         }
     }
     
@@ -50,7 +72,20 @@ public class RingBuffer {
             //return null;
         }
         else {
-            return values[last--];
+            if (firstToRead == lastAdded) {
+                int i = firstToRead;
+                firstToRead = -1;
+                lastAdded = -1;
+                return values[i];
+            }
+            else if (firstToRead == values.length - 1) {
+                firstToRead = 0;
+                return values[values.length - 1];   
+            }
+            else {
+                return values[firstToRead++];
+            }          
         }
     }
 }
+
