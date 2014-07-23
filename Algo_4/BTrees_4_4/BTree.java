ae8357ab247be0957c58453d6f59419997d33b9e
nekanek <inina.marina@gmail.com>
2014-07-23 21:58:52 +0400

    Binary tree

--- Algo_4/BTrees_4_4/BTree.java
+++ Algo_4/BTrees_4_4/BTree.java
@@ -30,16 +30,46 @@ public class BTree<V> {
     public BTree() {
         root = null;
     }
-    
     public BTree(int in_key, V in_value) {
         root = new Node(in_key, in_value);
     }
+
+    public void put(int key, V value) {
+        put(root, key, value);
+    }
+    private void put(Node aNode, int key, V value) {
+        if (aNode == null) {
+            aNode = new Node(key, value);
+        }
+        else if (key < aNode.key) {
+            put(aNode.left, key, value);
+        }
+        else if (key > aNode.key) {
+            put(aNode.right, key, value);
+        }
+        else aNode.value = value;
+    }        
     
+    public V get(int key) {
+        return get (root, key);
+    }
+    private V get(Node aNode, int key) {
+        if (aNode == null) {
+            return null;
+        }
+        else if (key < aNode.key) {
+            return get(aNode.left, key);
+        }
+        else if (key > aNode.key) {
+            return get(aNode.right, key);
+        }
+        else return aNode.value;
+    }    
+
     public int size() {
         return size(this.root);
     }
-    
-    private static int size(Node _root) {
+    private int size(Node _root) {
         int size = 1;
         if(_root.left != null) {
             size += size(_root.left);
@@ -53,9 +83,7 @@ public class BTree<V> {
     public int leaves() {
         return leaves(this.root);
     }
-    
-    
-    private static int leaves(Node _root) {
+    private int leaves(Node _root) {
         int leaves = 0;
         if (_root.left == null && _root.right == null) {
             leaves++;
@@ -74,8 +102,7 @@ public class BTree<V> {
     public int total() {
         return total(this.root);
     }    
-    
-    private static int total(Node _root) {
+    private int total(Node _root) {
         int sum = _root.key;
         if(_root.left != null) {
             sum += total(_root.left);
@@ -87,45 +114,20 @@ public class BTree<V> {
     }   
 
     public int height() {
-        return height(this.root);
+        return height(this.root, 0, 0);
     }
-    
-    
-    private static int height(Node _root) {
-        if (_root == null) {
-            return 0;
+    private int height(Node aNode, int currentMax, int globalMax) {
+        if (aNode == null) {
+            return globalMax;
+        }
+        else if (aNode.left == null && aNode.right == null) {
+            return Math.max(++currentMax, globalMax);
         }
         else {
-            int[] height = new int[10];
-            int[0] = 1;
-            int paths = 1; // number of different pathes in the tree
-            currentLeft = _root.left;
-            currentRight = _root.right;
-            while (currentLeft != null || currentRight!= null) {
-                if (paths > height.length -1) {
-                    // resize array 
-                    new int[];
-                    // copy
-                }
-                
-                if (currentLeft != null && currentRight != null) {
-                    paths ++;
-                    height[i]++;
-                    height[i+1] = height[i];
-                    currentLeft = this.left;
-                    currentRight = currentRight.right
-                }
-                else if (currentLeftt != null) {
-                }
-                else if (currentRight != null)
-            }
+            currentMax ++;
+            globalMax = this.height(aNode.left, currentMax, globalMax);
+            globalMax = height(aNode.right, currentMax, globalMax);
+            return globalMax;
         }
-        
-        // determine max
-        for {
-
-        }
-
-        return leaves;
-    }
+    }   
 }    
