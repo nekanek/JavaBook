0f2edf997aa037ecdd433a05d19dd55517d39d35
nekanek <inina.marina@gmail.com>
2014-07-24 17:10:55 +0400

    Majority done with better algo

--- Algo_4/Sorting_4_2/ex_4_2_26_Majority.java
+++ Algo_4/Sorting_4_2/ex_4_2_26_Majority.java
@@ -1,39 +1,36 @@
 // 26. Majority. An element is a majority if it appears more than N/2 times. Write a static method that takes an array of N strings as argument and identifies a majority (if it exists) in linear time. 
 // will print out last string if both of them r 50%
 
-package JavaBook.Algo_4.4_2_Sorting;
+package JavaBook.Algo_4.Sorting_4_2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
 public class ex_4_2_26_Majority {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         
         String[] input = scanStringArray();
-        // Merge.sort(input);
-         
         int majorityCounter = 0;
-        // String MajorityValue = "";
-        // int Majority = (int)input.length/2;
-        // boolean Found = false;
         String majorityString = null;
         
-        for(int i = 0; i < input.length; i++) {
-            
-            if (input[i].equals(majorityString)) 
+        for (String i : input) {
+            System.out.println("i " + i);
+            if (majorityCounter == 0) { 
+                majorityString = i;
                 majorityCounter++;
-            else if (majorityCounter > 0) {
+            }
+            else if (i.equals(majorityString)) {
+                majorityCounter++;
+            }
+            else  {
                 majorityCounter--;
-                //if (majorityCounter == 0)
-                //    majorityString = null;
             }
-            else if (majorityCounter == 0) {
-                majorityString = input[i];
-                majorityCounter++;
-            }
+            System.out.println("majorityCounter " + majorityCounter);
+            System.out.println("majorityString " + majorityString);
         }
-        
 
         if (majorityString != null) {
             System.out.println("Majority of input is string " + majorityString);
@@ -41,30 +38,15 @@ public class ex_4_2_26_Majority {
         else {
             System.out.println("no majority string found in the input.");
         }    
-        
     }    
 
-    public static String[] scanStringArray() {
-        Scanner sc = new Scanner(System.in);
-        int n;
-        System.out.println("Please, enter length of array as first element (int).");
-        if(sc.hasNextInt()) {
-          n = sc.nextInt();
-          sc.nextLine();
-        } 
-        else {
-          return scanStringArray();
-        }
-        
+    private static String[] scanStringArray() throws FileNotFoundException {
+        Scanner in = new Scanner(new File("JavaBook/Algo_4/Sorting_4_2/majority.txt"));
+        int n = in.nextInt(); //length of array
+        in.nextLine();
         String[] input = new String[n];
         for (int i = 0; i < n; i++) {
-            if(sc.hasNext()) {
-              input[i] = sc.nextLine(); 
-            } 
-            else {
-              System.out.println("Input was wrong. Please input again."); 
-              return scanStringArray();
-            }
+            input[i] = in.nextLine(); 
         }
         return input;
     } 
